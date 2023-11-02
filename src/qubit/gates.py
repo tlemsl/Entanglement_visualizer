5da7ce413e0eb9a661577301781554a0fc1edb3d
tlemsl <tlemsl8963@gmail.com>
2023-11-03 01:10:42 +0900

    feat : Introduce gates
    Introduce Z, X, and Hadamarda gates
     - Add Base class
     - Add Z class
     - Add X class
     - Add H class

---
+++ src/qubit/gates.py
@@ -0,0 +1,178 @@
+"""
+Quantum Gates and Operations
+
+This module defines quantum gates and operations for quantum computing. It includes the
+base quantum gate class `Base`, X gate `X`, Z gate `Z`, and Hadamard gate `H`.
+
+Author: Minjong Kim
+Email: tlemsl@dgist.ac.kr
+Website: https://github.com/tlemsl/Entanglement_visualizer
+
+Classes:
+    Base: Base class representing a quantum gate.
+    X: Class representing an X gate.
+    Z: Class representing a Z gate.
+    H: Class representing an H (Hadamard) gate.
+"""
+
+import math
+import numpy as np
+import qubit.qubit as qb
+
+Identity_matrix = np.identity(2, dtype=np.complex128)
+
+class Base(object):
+    """Base class representing a quantum gate.
+
+    This class serves as the base for quantum gate implementations.
+
+    Attributes:
+        _n (int): The number of qubits.
+        _target (int): The target qubit index.
+        _base_mat (numpy.ndarray): The base matrix representing the gate.
+    """
+
+    def __init__(self, n: int = 1, target: int = 0) -> None:
+        """Initialize a base quantum gate.
+
+        Args:
+            n (int, optional): The number of qubits (default is 1).
+            target (int, optional): The target qubit index (default is 0).
+
+        Raises:
+            ValueError: If the target qubit is not smaller than n.
+        """
+        if n <= target:
+            raise ValueError(f"Target({target}) must be smaller than n({n})")
+        self._base_mat = Identity_matrix
+        self._n = n
+        self._target = target
+        self._mat = self._form_matrix()
+
+    def __mul__(self, other):
+        """Multiply two quantum gates using matrix multiplication.
+
+        Args:
+            other (Base or qb.Qubit): The other gate or qubit to multiply with.
+
+        Returns:
+            Base or qb.Qubit: A new gate or qubit representing the result of the multiplication.
+        """
+        if isinstance(other, qb.Qubit):
+            ret = qb.Qubit()
+        else:
+            ret = Base()
+        ret._mat = np.dot(self.mat, other.mat)
+        return ret
+
+    def __len__(self):
+        """Get the number of qubits affected by the gate.
+
+        Returns:
+            int: The number of qubits.
+        """
+        return self._n
+    
+    def __str__(self) -> str:
+        return self._mat.__str__()
+
+    @property
+    def mat(self):
+        """Getter for the gate's matrix form.
+
+        Returns:
+            numpy.ndarray: The matrix representation of the gate.
+        """
+        return self._mat
+
+    @mat.setter
+    def mat(self, data):
+        """Setter for the gate's matrix form.
+
+        Args:
+            data (numpy.ndarray): The new matrix.
+
+        Raises:
+            ValueError: If the input matrix is not a valid square matrix.
+        """
+        if data.shape[0] == data.shape[1]:
+            raise ValueError("Matrix must be a square matrix")
+        self._mat = data
+        self._n = int(math.sqrt(self._mat.shape[0]))
+
+    def _form_matrix(self):
+        """Form the matrix representation of the gate.
+
+        Returns:
+            numpy.ndarray: The matrix representation of the gate.
+        """
+        ret = Identity_matrix
+        if self._target == (self._n - 1):
+            ret = self._base_mat
+        for i in range(self._n - 2, -1, -1):
+            if i == self._target:
+                ret = np.kron(ret, self._base_mat)
+            else:
+                ret = np.kron(ret, Identity_matrix)
+        return ret
+
+class X(Base):
+    """Class representing an X gate.
+
+    This class inherits from the Base class and represents the X gate.
+
+    Attributes:
+        _base_mat (numpy.ndarray): The base matrix representing the X gate.
+    """
+
+    def __init__(self, n: int = 1, target: int = 0) -> None:
+        """Initialize an X gate.
+
+        Args:
+            n (int, optional): The number of qubits (default is 1).
+            target (int, optional): The target qubit index (default is 0).
+        """
+        super().__init__(n, target)
+        self._base_mat = np.array([[0, 1], [1, 0]], dtype=np.complex128)
+        self._mat = self._form_matrix()
+
+class Z(Base):
+    """Class representing a Z gate.
+
+    This class inherits from the Base class and represents the Z gate.
+
+    Attributes:
+        _base_mat (numpy.ndarray): The base matrix representing the Z gate.
+    """
+
+    def __init__(self, n: int = 1, target: int = 0) -> None:
+        """Initialize a Z gate.
+
+        Args:
+            n (int, optional): The number of qubits (default is 1).
+            target (int, optional): The target qubit index (default is 0).
+        """
+        super().__init__(n, target)
+        self._base_mat = np.array([[1, 0], [0, -1]], dtype=np.complex128)
+        self._mat = self._form_matrix()
+
+class H(Base):
+    """Class representing an H gate.
+
+    This class inherits from the Base class and represents the H (Hadamard) gate.
+
+    Attributes:
+        _base_mat (numpy.ndarray): The base matrix representing the H gate.
+    """
+
+    def __init__(self, n: int = 1, target: int = 0) -> None:
+        """Initialize an H gate.
+
+        Args:
+            n (int, optional): The number of qubits (default is 1).
+            target (int, optional): The target qubit index (default is 0).
+        """
+        super().__init__(n, target)
+        temp = 1 / math.sqrt(2)
+        self._base_mat = np.array([[temp, temp], [temp, -temp]], dtype=np.complex128)
+        self._mat = self._form_matrix()
---
+++ tests/test_gates.py
@@ -0,0 +1,50 @@
+import unittest
+import math
+import numpy as np
+import qubit.gates as qg
+import qubit.qubit as qb
+
+class TestQuantumGates(unittest.TestCase):
+    def test_base_gate(self):
+        base_gate = qg.Base(2, 0)
+        qubit = qb.Qubit(2, 0)
+        result = base_gate * qubit
+        expected = qb.Qubit(2, 0)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_x_gate(self):
+        x_gate = qg.X(2, 0)
+        qubit = qb.Qubit(2, 0)
+        result = x_gate * qubit
+        expected = qb.Qubit(2, 1)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_z_gate(self):
+        z_gate = qg.Z(2, 0)
+        qubit = qb.Qubit(2, 1)
+        result = z_gate * z_gate * qubit
+        expected = qb.Qubit(2, 1)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_h_gate(self):
+        h_gate = qg.H(1, 0)
+        qubit = qb.Qubit(1, 0)
+        result = h_gate * qubit
+        expected = qb.Qubit()
+        expected.mat = np.array([[1],[1]], dtype=np.complex128) / math.sqrt(2)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_multiple_gates(self):
+        x_gate = qg.X(2, 0)
+        z_gate = qg.Z(2, 1)
+        h_gate = qg.H(2, 0)
+
+        qubit = qb.Qubit(2, 0)
+        result = z_gate * h_gate * x_gate * qubit
+
+        expected = qb.Qubit(2, 0)
+        expected.mat = np.array([[0.70710678+0.j], [-0.70710678+0.j], [0], [0]], dtype=np.complex128)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+        
+if __name__ == '__main__':
+    unittest.main()
