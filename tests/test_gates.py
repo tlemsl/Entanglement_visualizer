f05fb10968b7ebfb523af6b8d7637d62d9b0db64
Minjong Kim <50408036+tlemsl@users.noreply.github.com>
2023-11-05 23:19:04 +0900

    feat : Add control gates (#21)
    - Add control gates for all the quantum gate operations.
     - Add y gate missing docstrings in the gates.py file
     - Add unit test codes for Y-gate and Controlled-X, Y, Z, H gates.
     - Fix tests/test_qubit.py's unmerged code bases
     ```
     qtuser@c0cf44643ccc:~/Entanglement_visualizer$ python3 setup.py test
    running test
    test_base_gate (test_gates.TestQuantumGates) ... ok
    test_controlled_h_gate (test_gates.TestQuantumGates) ... ok
    test_controlled_x_gate (test_gates.TestQuantumGates) ... ok
    test_controlled_z_gate (test_gates.TestQuantumGates) ... ok
    test_h_gate (test_gates.TestQuantumGates) ... ok
    test_multiple_gates (test_gates.TestQuantumGates) ... ok
    test_x_gate (test_gates.TestQuantumGates) ... ok
    test_y_gate (test_gates.TestQuantumGates) ... ok
    test_y_gate_squared (test_gates.TestQuantumGates) ... ok
    test_z_gate (test_gates.TestQuantumGates) ... ok
    test_Conjugate_function (test_qubit.TestQubit)
    Test the T property for correct conjugate transpose. ... ok
    test_add (test_qubit.TestQubit)
    Test element-wise addition of two qubits. ... ok
    test_init (test_qubit.TestQubit)
    Test Qubit initialization with default/specific values. ... ok
    test_mat_property (test_qubit.TestQubit)
    Test qubit's state matrix property getters/setters. ... ok
    test_str (test_qubit.TestQubit)
    Test string representation of a qubit. ... ok
    test_sub (test_qubit.TestQubit)
    Test element-wise subtraction of two qubits. ... ok
    test_tensor_product (test_qubit.TestQubit)
    Test tensor product of two qubits for correctness. ... ok
    
    ----------------------------------------------------------------------
    Ran 17 tests in 0.019s
    
    OK
    ```

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -50,6 +50,55 @@ class TestQuantumGates(unittest.TestCase):
             dtype=np.complex128)
         self.assertTrue(np.allclose(result.mat, expected.mat))
 
+    def test_y_gate(self):
+        y_gate = qg.Y(2, 0)
+        qubit = qb.Qubit(2, 1)  # |01>
+        result = y_gate * qubit
+        expected = qb.Qubit(2, 0)  # Expected to be -|00> after Y gate
+        # Y gate applies iσy, so the result should be multiplied by i
+        expected.mat = np.array([[-1j], [0], [0], [0]], dtype=np.complex128)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_y_gate_squared(self):
+        y_gate = qg.Y(1, 0)
+        qubit = qb.Qubit(1, 0)  # |0>
+        result = y_gate * y_gate * qubit
+        # Applying Y gate twice should be equivalent to applying a I.
+        expected = qb.Qubit(1, 0)  # |0>
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_controlled_x_gate(self):
+        cx_gate = qg.X(
+            n=2, target=0,
+            control=1)  # CNOT with control on qubit 0 and target on qubit 1
+        qubit = qb.Qubit(2, 0b10)  # |10>
+        result = cx_gate * qubit
+        expected = qb.Qubit(2, 0b11)  # Should be |11> after CNOT
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_controlled_z_gate(self):
+        cz_gate = qg.Z(
+            n=2, target=0,
+            control=1)  # CZ with control on qubit 0 and target on qubit 1
+        qubit = qb.Qubit(2, 0b11)  # |11>
+        result = cz_gate * qubit
+        # CZ flips the phase of the target qubit if the control qubit is |1>
+        expected = qb.Qubit(2, 0b11)
+        expected.mat = np.array([[0], [0], [0], [-1]], dtype=np.complex128)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
+    def test_controlled_h_gate(self):
+        ch_gate = qg.H(
+            n=2, target=0,
+            control=1)  # CH gate with control on qubit 0 and target on qubit 1
+        qubit = qb.Qubit(2, 0b10)  # |10>
+        result = ch_gate * qubit
+        # CH should apply Hadamard on the target qubit if the control qubit is |1>
+        expected = qb.Qubit(2, 0b10)
+        expected.mat = np.array([[0], [0], [1], [1]],
+                                dtype=np.complex128) / math.sqrt(2)
+        self.assertTrue(np.allclose(result.mat, expected.mat))
+
 
 if __name__ == '__main__':
     unittest.main()
