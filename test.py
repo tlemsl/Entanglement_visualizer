589c2b353bc9fda3bcf6fc77832ec5f22154ff2c
ChanyuMoon <moonchanyu@gmail.com>
2023-11-12 18:49:21 +0900

    test: changed the qubit, gate test file

--- test.py
+++ test.py
@@ -6,14 +6,28 @@ import qubit.qubit as qb
 
 
 Q = qb.Qubit(2, 0)
-X0 = qg.X(2,0)
-Z1 = qg.Z(2,1)
-H0 = qb.H(2,0)
 
-G1 = X0 * Z1
-G2 = H0 * Z1
-G3 = H0
+# X0 = qg.X(2,0)
+H0 = qg.H(2,0)
 
-print(G1 * Q)
-print(G2 * G1 * Q)
-print(G3* G2 * G1 * Q)
+H1 = qg.H(2,1)
+
+# G0 = X0
+G1 = H0 * H1
+
+print(f"H0:\n{H0}")
+print(f"H1:\n{H1}")
+
+print(len(G1*Q))
+print(G1*Q)
+
+print(f"H0*Q:{H0*Q}")
+
+print(f"Q: {Q}")
+
+# print(Q)
+# print("len:", len(Q))
+# print(G0*Q)
+# print("len:", len(G0*Q))
+# print(G1*G0*Q)
+# print("len:", len(G1*G0*Q))
